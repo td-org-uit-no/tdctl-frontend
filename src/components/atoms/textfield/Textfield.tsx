78e0200d097672d07fd85f743410145f82450594
Jon Johansen <jonjohansen2k@gmail.com>
2020-12-05 21:27:55 +0100

    :sparkles: Update textfield to support label and errors"

--- src/components/atoms/textfield/Textfield.tsx
+++ src/components/atoms/textfield/Textfield.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
+import styles from './textfield.module.scss';
 
-interface Props extends React.HTMLAttributes<HTMLInputElement> {}
-const TextField: React.FC<Props> = ({ ...rest }) => {
-  return <input {...rest} />;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  maxWidth?: number;
+  error?: string[] | undefined;
+}
+
+const TextField: React.FC<Props> = ({ error, maxWidth, label, ...rest }) => {
+  if (label) {
+    return (
+      <div
+        className={styles.fieldWrapper}
+        style={{ maxWidth: maxWidth ? maxWidth + 'ch' : '' }}>
+        <label>{label}</label>
+        <input className={styles.textField} {...rest} />
+        {error && <div>{error.map((error) => error)}</div>}
+      </div>
+    );
+  }
+
+  return <input className={styles.textfield} {...rest} />;
 };
 
 export default TextField;
