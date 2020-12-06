876d405b2b3950d00212b5be11095e492581de64
Jon Johansen <jonjohansen2k@gmail.com>
2020-12-06 03:23:47 +0100

    :sparkles: Login now sets authentication. Navbar consumes the context to display login status

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Authenticated } from '../../../contexts';
 import styles from './navbar.module.scss';
 
 const Navbar: React.FC = () => {
-  return <div className={styles.navbar}>Hei dette er en navbar</div>;
+  const { authenticated } = useContext(Authenticated);
+  return (
+    <div className={styles.navbar}>
+      Hei. {authenticated ? 'Du er logget inn' : 'Du er ikke logget inn'}
+    </div>
+  );
 };
 
 export default Navbar;
