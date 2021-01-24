eee25554e967b576efe3e130b5e0d926cf12193e
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-01-24 21:57:04 +0100

    :sparkles: add a simple dropdown menu component

---
+++ src/components/atoms/dropdown/Dropdown.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import styles from './dropdown.module.scss';
+import Button from '../button/Button';
+import cx from 'classnames';
+
+interface Props {
+    items: {
+        lable: string,
+        ref: string, 
+    }[];
+}
+const Dropdown: React.FC<Props> = ({ items }) => {
+    const [active, setActive] = useState(false);
+
+    return(
+        <div className={styles.menuContainer}>
+            <Button version = {'secondary'} onClick={() => setActive(!active)}>
+                Menu
+            </Button>
+            <nav className={cx(styles.menu, {[styles.active]: active})}>
+                <ul>
+                    {items.map((item, index:number) => (
+                        <li key={index}><a href={item.ref}>{item.lable}</a></li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Dropdown
