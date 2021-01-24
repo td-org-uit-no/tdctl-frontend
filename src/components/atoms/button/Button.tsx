f86936012343e36e8488014294c8abde5c8a6fef
Jon Johansen <jonjohansen2k@gmail.com>
2021-01-24 14:33:35 +0100

    :sparkles: Initial buttons with ugly styling

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,8 +1,17 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import classnames from 'classnames';
+import styles from './button.module.scss';
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  version: 'primary' | 'secondary';
+}
+const Button: React.FC<Props> = ({ children, version, className, ...rest }) => {
+  const classes = classnames(styles.btn, styles[version], className);
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
-const Button: React.FC<Props> = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>;
+  return (
+    <button className={classes} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
