42a1fb13d38bd7274a0dbb6c67f3ddd12c71ad5c
Jon Johansen <jonjohansen2k@gmail.com>
2021-01-24 17:21:17 +0100

    :lipstick: Adjust navbar items and show login link when not logged in"

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { Authenticated } from 'contexts';
 import styles from './navbar.module.scss';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   const { authenticated } = useContext(Authenticated);
   return (
     <div className={styles.navbar}>
-      Hei. {authenticated ? 'Du er logget inn' : 'Du er ikke logget inn'}
+      {!authenticated && <Link to="/login"> Logg inn</Link>}
     </div>
   );
 };
