5a32611f45dbb0d05a145a37912a09416e2d869a
Jon Johansen <jonjohansen2k@gmail.com>
2021-01-24 16:07:23 +0100

    :lipstick: move away from module.scss for buttons

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,11 +1,11 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import classnames from 'classnames';
-import styles from './button.module.scss';
+import './button.scss';
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   version: 'primary' | 'secondary';
 }
 const Button: React.FC<Props> = ({ children, version, className, ...rest }) => {
-  const classes = classnames(styles.btn, styles[version], className);
+  const classes = classnames(version, className);
 
   return (
     <button className={classes} {...rest}>
