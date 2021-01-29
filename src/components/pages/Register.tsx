585d9dae90d33fb57e230902a041be414f6f3a57
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-01-29 14:05:01 +0100

    :sparkles: Add a new register page using a register form

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import useTitle from 'hooks/useTitle';
+import RegistrerForm from '../molecules/Forms/RegisterForm/RegisterForm';
 
 const RegistrerPage = () => {
   useTitle('Bli medlem - Tromsøstudentenes Dataforening');
   return (
     <div>
       <h1> Registrer deg og bli medlem!</h1>
+      <div>
+        <RegistrerForm />
+      </div>
     </div>
   );
 };
