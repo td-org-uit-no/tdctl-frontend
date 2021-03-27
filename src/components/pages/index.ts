8c380a130f652e9f619ede9f56342a833039d83d
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-03-27 11:54:56 +0100

    :sparkles: add a simple profile page

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,5 +1,6 @@
 import RegistrerPage from './Register';
 import RootPage from './Root';
+import ProfilePage from './profile/Profile';
 import LoginPage from './login/Login';
 
-export { RegistrerPage, RootPage, LoginPage };
+export { RegistrerPage, RootPage, LoginPage, ProfilePage };
