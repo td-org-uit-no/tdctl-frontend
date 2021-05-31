28c9d11799d1fc62dcb17de6e79db944b3d23e2c
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-05-31 20:02:14 +0200

    :sparkles: add support for initial values in useForm hook

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -11,17 +11,36 @@ interface Fields {
 interface Validators {
   [key: string]: (value: string) => string[] | undefined;
 }
+interface InitalValue {
+  [key: string]: string;
+}
 
-const useForm = (onSubmit: () => any, validators?: Validators) => {
-  const initialFields = !validators
+interface formProps {
+  onSubmit: () => any;
+  validators?: Validators;
+  initalValue?: InitalValue;
+}
+
+const useForm = ({ onSubmit, validators, initalValue }: formProps) => {
+  let initialFields = !validators
     ? {}
     : dictFromArray<Field>(Object.keys(validators), {
         value: '',
         error: undefined,
       });
 
+  // init field with values and validates the data
+  if (initalValue) {
+    Object.keys(initalValue).forEach((key) => {
+      const value = initalValue[key]
+      const fieldError = validators?.[key](value) ?? undefined;
+      initialFields[key] = {
+        value: initalValue[key],
+        error: fieldError,
+      };
+    });
+  }
   const [fields, setFields] = useState<Fields>(initialFields);
-
   /* Finding errors across the enitre form */
   const [hasErrors, setErrors] = useState(false);
 
@@ -32,7 +51,11 @@ const useForm = (onSubmit: () => any, validators?: Validators) => {
     setErrors(isThereErrors(Object.keys(fields).map((field) => fields[field])));
   }, [fields]);
 
-  const setFieldError = (name: string, value: string, fieldError: string[] | undefined) => {
+  const setFieldError = (
+    name: string,
+    value: string,
+    fieldError: string[] | undefined
+  ) => {
     setFields({
       ...fields,
       [name]: {
@@ -40,13 +63,13 @@ const useForm = (onSubmit: () => any, validators?: Validators) => {
         error: fieldError,
       },
     });
-
-  }
+  };
 
   const onFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    const fieldError = validators?.[name](value);
-	setFieldError(name, value, fieldError);
+    //Don't require all forms to have a validator
+    const fieldError = validators?.hasOwnProperty(name) ? validators?.[name](value) : undefined;
+    setFieldError(name, value, fieldError);
   };
 
   const onSubmitEvent = (event: any) => {
