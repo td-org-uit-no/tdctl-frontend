7776c0b6bba75e95b8528f53acf9523332012180
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2021-08-09 15:08:49 +0200

    :sparkles: add a page for creating events

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -3,5 +3,6 @@ import RootPage from './Root';
 import ProfilePage from './profile/Profile';
 import LoginPage from './login/Login';
 import SettingsPage from './settings/Settings'
+import CreateEvent from './events/CreateEvent/createEvent';
 
-export { RegistrerPage, RootPage, LoginPage, ProfilePage, SettingsPage};
+export { RegistrerPage, RootPage, LoginPage, ProfilePage, SettingsPage, CreateEvent};
