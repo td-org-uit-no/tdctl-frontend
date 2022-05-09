cc1a7f347c291ad803c9a0a857c8611da90fb5a1
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-09 12:54:07 +0200

    :art: fix formating

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,9 +1,11 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import classnames from 'classnames';
 import './button.scss';
+
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   version: 'primary' | 'secondary';
 }
+
 const Button: React.FC<Props> = ({ children, version, className, ...rest }) => {
   const classes = classnames(version, className);
 
