cf6cef3f2e1e3395fb4ea7689455e0443d1479f1
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-09 13:33:53 +0200

    :sparkles: add function to convert date to text

---
+++ src/utils/timeConverter.ts
@@ -0,0 +1,77 @@
+// TODO setup support for return format eg HH:mm:ss
+// returns day date time
+export const transformDate = (date: Date): string => {
+  const day = [
+    'Søndag',
+    'Mandag',
+    'Tirsdag',
+    'Onsdag',
+    'Torsdag',
+    'Fredag',
+    'Lørdag',
+  ][date.getDay()];
+  const month = [
+    'januar',
+    'februar',
+    'mars',
+    'april',
+    'mai',
+    'juni',
+    'juli',
+    'august',
+    'september',
+    'oktober',
+    'november',
+    'desember',
+  ][date.getMonth()];
+
+  const time =
+    ' ' +
+    date.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+    });
+
+  if (isToday(date)) {
+    return 'I dag' + time;
+  } else if (isTomorrow(date)) {
+    return 'I morgen' + time;
+  } else if (isYesterday(date)) {
+    return 'I går' + time;
+  }
+  return (
+    day + ' ' + date.getDate() + '.' + month + ' ' + time
+  );
+};
+
+const isToday = (date: Date) => {
+  const today = new Date();
+  return (
+    date.getDate() === today.getDate() &&
+    date.getMonth() === today.getMonth() &&
+    date.getFullYear() === today.getFullYear()
+  );
+};
+
+const isTomorrow = (date: Date) => {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+  return (
+    date.getDate() === tomorrow.getDate() &&
+    date.getMonth() === tomorrow.getMonth() &&
+    date.getFullYear() === tomorrow.getFullYear()
+  );
+};
+
+const isYesterday = (date: Date) => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  return (
+    date.getDate() === yesterday.getDate() &&
+    date.getMonth() === yesterday.getMonth() &&
+    date.getFullYear() === yesterday.getFullYear()
+  );
+};
