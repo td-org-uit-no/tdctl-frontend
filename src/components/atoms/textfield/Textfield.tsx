876a624502d4b7a0303120524367644bec5f3bd0
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-09 12:54:07 +0200

    :art: fix formating

--- src/components/atoms/textfield/Textfield.tsx
+++ src/components/atoms/textfield/Textfield.tsx
@@ -4,18 +4,34 @@ import styles from './textfield.module.scss';
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   maxWidth?: number;
+  minWidth?: number;
   error?: string[] | undefined;
 }
 
-const TextField: React.FC<Props> = ({ error, maxWidth, label, ...rest }) => {
+const TextField: React.FC<Props> = ({
+  error,
+  maxWidth,
+  minWidth,
+  label,
+  ...rest
+}) => {
   if (label) {
     return (
       <div
         className={styles.fieldWrapper}
-        style={{ maxWidth: maxWidth ? maxWidth + 'ch' : '' }}>
+        style={{
+          maxWidth: maxWidth ? maxWidth + 'ch' : '',
+          minWidth: minWidth ? minWidth + 'ch' : '',
+        }}>
         <label>{label}</label>
         <input className={styles.textField} {...rest} />
-        {error && <div>{error.map((err, index:number) => error.length > 1 ? <li key={index}>{err}</li> : err )}</div>}
+        {error && (
+          <div>
+            {error.map((err, index: number) =>
+              error.length > 1 ? <li key={index}>{err}</li> : err
+            )}
+          </div>
+        )}
       </div>
     );
   }
