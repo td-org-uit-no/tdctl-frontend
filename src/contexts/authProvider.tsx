ab438a553f4260a14d6c1c2733cff7e99550b69e
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-09 13:00:43 +0200

    :sparkles: add role field to authentication provider

--- src/contexts/authProvider.tsx
+++ src/contexts/authProvider.tsx
@@ -1,30 +1,48 @@
 import React, { useState, useEffect, createContext } from 'react';
-import { verifyAuthentication } from 'utils/auth';
+import { getRole, verifyAuthentication } from 'utils/auth';
 
 export const AuthenticateContext = createContext({
   authenticated: false,
   setAuthenticated: (authenticated: boolean) => {},
   isValidating: true,
+  role: "unconfirmed" as RoleOptions,
+  updateCredentials: () => {}
 });
+
+export type RoleOptions = 'unconfirmed' | 'member' | 'admin';
 
 const AuthenticateProvider: React.FC = ({ children }) => {
   const [authenticated, setAuthenticated] = useState(true);
   const [isValidating, setValidating] = useState(true);
+  const [role, setRole] = useState<RoleOptions>('unconfirmed');
 
-  const isAuth = () => {
+  const updateCredentials = () => {
     verifyAuthentication().then((res) => {
-      setAuthenticated(res);
-      setValidating(false);
+      if (res) {
+        try {
+          const userRole = getRole() as RoleOptions;
+          setRole(userRole);
+        } catch (error) {
+          setRole("unconfirmed");
+        }
+        setAuthenticated(res);
+        setValidating(false);
+      } else {
+        setValidating(true);
+        setRole("unconfirmed")
+        setAuthenticated(false)
+        setValidating(false);
+      }
     });
   };
 
   useEffect(() => {
-    isAuth();
+    updateCredentials();
   }, []);
 
   return (
     <AuthenticateContext.Provider
-      value={{ authenticated, setAuthenticated, isValidating }}>
+      value={{ authenticated, setAuthenticated, isValidating, role, updateCredentials}}>
       {children}
     </AuthenticateContext.Provider>
   );
