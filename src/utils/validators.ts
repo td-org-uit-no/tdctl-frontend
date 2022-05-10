7b9db41a899719f8b6e1d735d0d9b4da602ac0e7
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-05-10 11:53:44 +0200

    :sparkles: add max participants validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,4 +1,4 @@
-import {Fields} from 'hooks/useForm';
+import { Fields } from 'hooks/useForm';
 
 export const emailValidator = (email: string) => {
   const errors: string[] = [];
@@ -34,8 +34,19 @@ export const passwordValidator = (password: string): string[] | undefined => {
   return errors.length > 0 ? errors : undefined;
 };
 
+export const maxParticipantsValidator = (
+  maxParticipants: string | undefined
+): string[] | undefined => {
+  if (maxParticipants !== undefined) {
+    if (parseInt(maxParticipants) < 0) {
+      return ['max antall kan ikke være negativt'];
+    }
+  }
+  return undefined;
+};
+
 export const nameValidator = (name: string): string[] | undefined => {
-  const nameReg = /^[a-zæøåA-ZÆØÅ ]+$/ //! note the last space
+  const nameReg = /^[a-zæøåA-ZÆØÅ ]+$/; //! note the last space
 
   if (name.length === 0) {
     return ['Navn er påkrevd'];
@@ -47,7 +58,7 @@ export const nameValidator = (name: string): string[] | undefined => {
 };
 
 export const titleValidator = (name: string): string[] | undefined => {
-  const nameReg = /^[a-zæøåA-ZÆØÅ ]+$/ //! note the last space
+  const nameReg = /^[a-zæøåA-ZÆØÅ ]+$/; //! note the last space
 
   if (name.length === 0) {
     return ['Tittel er påkrevd'];
@@ -62,10 +73,10 @@ export const notRequiredNameValidator = (name: string): string[] | undefined =>
   !name.length ? undefined : nameValidator(name);
 
 export const classOfValidator = (year: string): string[] | undefined => {
-  const numbReg = /^[0-9]*$/
+  const numbReg = /^[0-9]*$/;
 
-  if (year.length !== 4){
-    return['Årskull: YYYY']
+  if (year.length !== 4) {
+    return ['Årskull: YYYY'];
   }
 
   if (!new RegExp(numbReg).test(year)) {
@@ -83,8 +94,9 @@ export const classOfValidator = (year: string): string[] | undefined => {
   return undefined;
 };
 
-export const notRequiredPhoneValidator = (phone: string): string[] | undefined =>
-  !phone.length ? undefined : phoneValidator(phone)
+export const notRequiredPhoneValidator = (
+  phone: string
+): string[] | undefined => (!phone.length ? undefined : phoneValidator(phone));
 
 export const phoneValidator = (num: string): string[] | undefined => {
   if (num.length !== 8) {
@@ -95,24 +107,24 @@ export const phoneValidator = (num: string): string[] | undefined => {
 
 export const addressValidator = (address: string) => {
   return address.length ? undefined : ['Adresse må fylles ut'];
-}
+};
 
 export const priceValidator = (price: string) => {
   return price.length ? undefined : ['Arrangement pris må fylles ut'];
-}
+};
 
 export const dateValidator = (date: string) => {
   return date.length ? undefined : ['Dato må fylles ut'];
-}
+};
 
 export const timeValidator = (time: string) => {
   return time.length ? undefined : ['Tidspunkt må fylles ut'];
-}
+};
 
 // description length constraint
 export const descriptionValidator = (description: string) => {
   return description.length >= 1000 ? ['Beskrivelse er for lang'] : undefined;
-}
+};
 
 interface InputFields {
   fields: Fields;
@@ -120,7 +132,9 @@ interface InputFields {
 }
 
 export const emptyFieldsValidator = ({ fields, optFields }: InputFields) => {
-  return Object.keys(fields).filter((key) => {
-    return !fields[key].value.length && !optFields?.includes(key);
-  }).length !== 0;
+  return (
+    Object.keys(fields).filter((key) => {
+      return !fields[key].value.length && !optFields?.includes(key);
+    }).length !== 0
+  );
 };
