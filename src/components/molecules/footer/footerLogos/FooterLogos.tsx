29843bc3cd9f258b85943f047a0a6d768deb79e6
Fredrik Mørstad <Fredrik.Morstad@hotmail.com>
2022-10-25 13:21:58 +0200

    :sparkles: add redirects to footer icons

--- src/components/molecules/footer/footerLogos/FooterLogos.tsx
+++ src/components/molecules/footer/footerLogos/FooterLogos.tsx
@@ -2,8 +2,12 @@ import styles from './footerLogos.module.scss';
 import { FaFacebook } from 'react-icons/fa';
 import { AiFillGithub } from 'react-icons/ai';
 import { Icon } from '@iconify/react';
+import Modal from 'components/molecules/modal/Modal';
+import { useState } from 'react';
+import discordInvite from 'assets/IFI-discord-invite.png';
 
 const FooterLogos = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <div className={styles.logosContainer}>
       <div className={styles.logosWrapper}>
@@ -11,24 +15,56 @@ const FooterLogos = () => {
           <div className={styles.logoItem}>
             <Icon
               icon="akar-icons:instagram-fill"
+              href="https::/google.com"
+              onClick={() => {
+                window.open('https://www.instagram.com/td.uit/', '_blank');
+              }}
               width={'2.5em'}
               height={'2.5em'}
-              color={"#F8D2CC"}
+              color={'#F8D2CC'}
             />
           </div>
           <div className={styles.logoItem}>
-            <AiFillGithub size={'2.5em'} color={'#F8D2CC'} />
+            <AiFillGithub
+              size={'2.5em'}
+              color={'#F8D2CC'}
+              onClick={() => {
+                window.open('https://github.com/td-org-uit-no', '_blank');
+              }}
+            />
           </div>
           <div className={styles.logoItem}>
             <Icon
               icon="simple-icons:discord"
               width={'2.5em'}
               height={'2.5em'}
-              color={"#F8D2CC"}
+              color={'#F8D2CC'}
+              onClick={() => {
+                setIsOpen(true);
+              }}
             />
+            {isOpen && (
+              <Modal
+                minWidth={45}
+                title="Invitasjon til vår discord"
+                setIsOpen={setIsOpen}>
+                <div>
+                  <img src={discordInvite} alt="Link to discord" />
+                </div>
+              </Modal>
+            )}
           </div>
           <div className={styles.logoItem}>
-            <FaFacebook size={'2.5em'} color={'#F8D2CC'} />
+            <FaFacebook
+              size={'2.5em'}
+              color={'#F8D2CC'}
+              onClick={() => {
+                window.open(
+                  'https://www.facebook.com/tromsodataforening',
+                  '_blank'
+                );
+              }}
+            />
           </div>
         </div>
       </div>
