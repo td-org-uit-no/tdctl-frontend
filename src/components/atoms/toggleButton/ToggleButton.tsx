384f3498d25ccb3d862b3ac2911bc2a73095cc01
Sagensagen <fsa028@uit.no>
2022-12-11 15:55:10 +0100

    :art: Add initial value prop to toggle

--- src/components/atoms/toggleButton/ToggleButton.tsx
+++ src/components/atoms/toggleButton/ToggleButton.tsx
@@ -3,22 +3,25 @@ import styles from './toggleButton.module.scss';
 interface Props {
   onChange: () => any;
   label: string;
+  initValue?: boolean;
 }
-const ToggleButton: React.FC<Props> = ({ onChange, label }) => {
+const ToggleButton: React.FC<Props> = ({ onChange, label, initValue }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.textContainer}>
-        <p className={styles.text}>{label}</p>
-      </div>
       <div className={styles.toggleContainer}>
         <label className={styles.switch}>
           <input
+            // checked={initValue}
+            defaultChecked={initValue}
             type="checkbox"
             className={styles.toggle}
             onChange={onChange}
           />
           <div className={styles.slider}></div>
         </label>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>{label}</p>
+        </div>
       </div>
     </div>
   );
