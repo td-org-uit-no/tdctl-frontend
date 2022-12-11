a27ec6ba5049766ebc94bfb90f9318fb7f11f90c
Sagensagen <fsa028@uit.no>
2022-12-11 15:54:08 +0100

    :art: Add color prop for icons

--- src/components/atoms/icons/icon.tsx
+++ src/components/atoms/icons/icon.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface Props extends React.HTMLAttributes<HTMLSpanElement> {
   type: string;
   size?: number;
+  color?: string;
 }
 
-const Icon = ({ type, size = 1, onClick }: Props) => (
+const Icon = ({ type, size = 1, color = 'white', onClick }: Props) => (
   <span
     className={`las la-${type}`}
-    style={{ fontSize: `${size}rem`, cursor: 'pointer' }}
+    style={{ fontSize: `${size}rem`, cursor: 'pointer', color: color }}
     onClick={onClick}
   />
 );
 export default Icon;
+<p>Test</p>;
