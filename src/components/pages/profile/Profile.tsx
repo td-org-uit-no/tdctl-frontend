5cca881354555125af4d51fe6a036a5c2a32ccc5
Vetle Hofsøy-Woie <vho023@uit.no>
2022-12-29 17:12:37 +0100

    :sparkles: Add notification to profile page if user is not confirmed.

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react';
 import styles from './profile.module.scss';
 import useTitle from 'hooks/useTitle';
-import { PartialMember } from 'models/apiModels';
-import { getMemberAssociatedWithToken } from 'api';
+import { Member, PartialMember } from 'models/apiModels';
+import { getMemberAssociatedWithToken,sendNewVerificationEmail } from 'api';
+import Button from 'components/atoms/button/Button';
 
 const ProfilePage = () => {
-  const [data, setData] = useState<PartialMember>();
+  const [data, setData] = useState<Member>();
   useTitle('Min profil');
 
   useEffect(() => {
     (async function getUserInfo() {
-      const response = (await getMemberAssociatedWithToken()) as PartialMember;
+      const response = (await getMemberAssociatedWithToken()) as Member;
       setData(response);
     })();
   }, []);
+  
   const items = [
     { lable: 'Navn', data: data?.realName },
     { lable: 'E-post', data: data?.email },
@@ -40,6 +42,16 @@ const ProfilePage = () => {
           )}
         </div>
       </div>
+      {data?.role == "unconfirmed" &&
+        <div>
+          <p>
+            Seems like you have not verified your user, please click on the verification link sent to your email address
+          </p>
+          <Button version='secondary' onClick={async () => { await sendNewVerificationEmail(data.email)}}>
+            Click here to get new verification email.
+          </Button>
+        </div>
+      }
     </div>
   );
 };
