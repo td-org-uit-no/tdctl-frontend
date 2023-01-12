46ba6e02a47c9bc72dc18b39c0e298d2a373d3e5
alex <alexstim@hotmail.no>
2023-01-12 12:26:25 +0100

    :lipstick: add ui for showing carousel on mobile devices and touch and swipe feature

--- src/components/pages/homepage/HomePage.tsx
+++ src/components/pages/homepage/HomePage.tsx
@@ -6,12 +6,13 @@ import Carousel from 'components/molecules/carousel/Carousel';
 import EventPreview from 'components/molecules/event/eventPreview/EventPreview';
 import useUpcomingEvents from 'hooks/useEvents';
 import LoadingWrapper from 'components/atoms/loadingWrapper/LoadingWrapper';
+import { useMobileScreen } from 'hooks/useCheckMobileScreen';
 
 const RootPage = () => {
   const { events } = useUpcomingEvents();
   useTitle('Tromsøstudentenes-Dataforening');
-
-  return (
+  const isMobile = useMobileScreen(); 
+  return ( 
     <div className={styles.root}>
       <HomeHeader />
       <div className={styles.eventsContainer}>
@@ -20,22 +21,24 @@ const RootPage = () => {
             data={events}
             animation={false}
             className={styles.wrapper}>
-            {events.length ? (
-              <Carousel
-                title="Arrangementer"
-                dir="row"
-                viewItems={3}
-                spacing={true}
-                height={'45vh'}>
-                {events.map((event) => (
-                  <EventPreview eventData={event} key={event.eid} />
-                ))}
-              </Carousel>
-            ) : (
-              <h3 style={{ minHeight: '45vh' }}>
-                Ingen kommende arrangementer{' '}
-              </h3>
-            )}
+              {events.length ? (
+                <div>
+                  <Carousel
+                    title="Arrangementer"
+                    dir={isMobile ? "column" : "row"}
+                    viewItems={isMobile ? 1 : 3}
+                    spacing={true}
+                    height={'45vh'}>
+                    {events.map((event) => (
+                      <EventPreview eventData={event} key={event.eid} />
+                    ))}
+                  </Carousel>
+                </div>
+              ) : (
+                <h3 style={{ minHeight: '45vh' }}>
+                  Ingen kommende arrangementer{' '}
+                </h3>
+              )}
           </LoadingWrapper>
         </div>
       </div>
