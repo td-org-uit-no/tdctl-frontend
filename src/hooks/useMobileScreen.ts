ef42982dcfd879ddd6599cb36a0e7a6afedbfb57
alex <alexstim@hotmail.no>
2023-01-12 12:24:39 +0100

    :sparkles: add hook for checking browser width

---
+++ src/hooks/useMobileScreen.ts
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+
+export const useMobileScreen = () => {
+    const mobileThreshold = 768;
+    const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= mobileThreshold);
+        
+    const handleWindowSizeChange = () => {
+        setIsMobile(window.innerWidth <= mobileThreshold); 
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    return isMobile
+}
