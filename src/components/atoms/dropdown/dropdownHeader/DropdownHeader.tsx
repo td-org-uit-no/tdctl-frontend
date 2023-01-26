0990ea7e134b825e95cd0f98c85308af9eaea5ca
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-01-26 16:35:10 +0100

    :lipstick: update dropdown logo

--- src/components/atoms/dropdown/dropdownHeader/DropdownHeader.tsx
+++ src/components/atoms/dropdown/dropdownHeader/DropdownHeader.tsx
@@ -1,33 +1,26 @@
 import React, { useState } from 'react';
 import styles from './dropdownHeader.module.scss';
 import Dropdown from 'components/atoms/dropdown/Dropdown';
-import icon from 'assets/menu-icon.png';
+import Icon from 'components/atoms/icons/icon';
 
-interface Props {
+interface Props extends React.HtmlHTMLAttributes<HTMLDivElement> {
   title: string;
 }
 
-const DropdownHeader: React.FC<Props> = ({ title, children }) => {
+const DropdownHeader: React.FC<Props> = ({ title, children, ...rest }) => {
   const [expanded, setExpanded] = useState(false);
 
   const onExpand = () => setExpanded(!expanded);
 
   return (
     <div className={styles.container}>
-      <div className={styles.box}>
+      <div className={styles.box} {...rest}>
         <div className={styles.base} onClick={onExpand}>
           <div className={styles.item2}>
             <p>{title}</p>
           </div>
           <div className={styles.item3}>
-            <img
-              alt="Logo"
-              src={icon}
-              style={{
-                height: '20px',
-                width: '30px',
-              }}
-            />
+            <Icon type={'angle-double-down'} size={2} color={'#80a2e1'} />
           </div>
         </div>
         <Dropdown expanded={expanded}>{children}</Dropdown>
