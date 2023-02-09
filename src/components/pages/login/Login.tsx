8e22fe1c6cc06c01147918029244d74161496cd7
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-02-09 14:19:17 +0100

    :sparkles: add title to login page

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { LoginForm } from 'components/molecules/forms/';
 import './login.scss';
+import useTitle from 'hooks/useTitle';
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
+  useTitle('Login');
   return (
     <div className="login-container">
       <div className="login-wrapper">
