06e8b1323b77746b2d106106ea2a031455eaaa69
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-02-17 15:28:38 +0100

    :sparkles: add the possibility to delete events

--- src/components/molecules/tables/eventTable/eventTable.tsx
+++ src/components/molecules/tables/eventTable/eventTable.tsx
@@ -5,14 +5,80 @@ import { useHistory } from 'react-router-dom';
 import Icon from 'components/atoms/icons/icon';
 import styles from './eventTable.module.scss';
 import { transformDate } from 'utils/timeConverter';
+import { useEffect, useState } from 'react';
+import Modal from 'components/molecules/modal/Modal';
+import ConformationBox from 'components/molecules/confirmationBox/ConfirmationBox';
+import useConfirmation from 'hooks/useConfirmation';
+import { deleteEvent } from 'api';
+import { useToast } from 'hooks/useToast';
 
 const EventTable = () => {
-  const { events } = useUpcomingEvents();
+  const { events, error, setFetch } = useUpcomingEvents();
+  const [deleteModal, setOpenDeleteModal] = useState(false);
+  const [selectedEvent, setSelected] = useState<
+    Pick<Event, 'eid' | 'title'> | undefined
+  >();
   const history = useHistory();
+  const { addToast } = useToast();
 
   const moveToEventAdminPage = (eid: string) => {
     history.push(`event/${eid}/admin`);
   };
+
+  const openDeleteColumn = (eid: string) => {
+    const selected = events?.find((mem) => {
+      return mem.eid === eid;
+    });
+    setSelected(selected);
+    setOpenDeleteModal(true);
+  };
+
+  const adminDeleteEvent = async () => {
+    setOpenDeleteModal(true);
+    try {
+      if (confirmed === false) {
+        setOpenDeleteModal(false);
+        return;
+      }
+      if (!selectedEvent) {
+        return;
+      }
+      await deleteEvent(selectedEvent.eid);
+      addToast({
+        title: 'Suksess',
+        status: 'success',
+        description: `${selectedEvent?.title} er slettet`,
+      });
+    } catch (error) {
+      // if another admin deletes the event
+      if (error.statusCode === 404) {
+        addToast({
+          title: 'Kunne ikke finne arrangement',
+          status: 'error',
+          description: `kunne ikke finne ${selectedEvent?.title}. Prøv å oppdater siden`,
+        });
+      }
+      addToast({
+        title: 'En uforutsett feil skjedde',
+        status: 'error',
+        description: 'Problemer med slettingen av arrangementet',
+      });
+    }
+    setFetch(true);
+    setOpenDeleteModal(false);
+  };
+
+  useEffect(() => {
+    // display error toast on error caught by useUpcomingEvents hook
+    if (error !== null) {
+      addToast({
+        title: 'Kunne ikke laste inn arrangementetene',
+        status: 'error',
+      });
+    }
+  }, [error]);
+
+  const { confirmed, setConfirmed } = useConfirmation(adminDeleteEvent);
 
   const columns: ColumnDefinitionType<Event, keyof Event>[] = [
     { cell: 'title', header: 'Name', type: 'string' },
@@ -65,12 +131,44 @@ const EventTable = () => {
       header: 'Edit',
       type: 'string',
     },
+    {
+      cell: (cellValues) => {
+        const { eid } = cellValues;
+        return (
+          <>
+            <Icon
+              size={2}
+              type="trash"
+              onClick={() => {
+                openDeleteColumn(eid);
+              }}
+            />
+          </>
+        );
+      },
+      header: 'Delete',
+    },
   ];
 
   return (
     <>
       <div className={styles.form}>
         <Table columns={columns} data={events}></Table>
+        {deleteModal && (
+          <Modal
+            title={`Are you sure you want to delete ${selectedEvent?.title}?`}
+            setIsOpen={setOpenDeleteModal}
+            minWidth={45}>
+            <ConformationBox
+              onAccept={() => {
+                setConfirmed(true);
+              }}
+              onDecline={() => {
+                setConfirmed(false);
+              }}
+            />
+          </Modal>
+        )}
       </div>
     </>
   );
