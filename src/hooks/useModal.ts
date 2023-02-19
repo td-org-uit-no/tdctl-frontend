016c5c392c6b1a70c76353bf683b86a9f8b2b1ca
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-02-19 16:39:18 +0100

    :fishing_pole_and_fish: add hook for using modals

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+// hook for opening and closing modals
+const useModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpen = () => {
+    setIsOpen(true);
+  };
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  return { isOpen, onOpen, onClose };
+};
+
+export default useModal;
