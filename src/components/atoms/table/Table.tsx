227bfed19189591fa4eb567a42f0edb05e2ec86d
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-03-20 18:08:09 +0100

    :sparkles: add possibility to set data from table component and add smaller features
    Co-authored-by: seblj <seblyng98@gmail.com>

--- src/components/atoms/table/Table.tsx
+++ src/components/atoms/table/Table.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState } from 'react';
+import {
+  useEffect,
+  useState,
+  DragEvent,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import { SortOption, typeToFunction } from 'utils/sorting';
 import './table.scss';
 // extends https://www.bekk.christmas/post/2020/22/create-a-generic-table-with-react-and-typescript
@@ -14,19 +20,30 @@ export type ColumnDefinitionType<T, K extends keyof T> = {
 
 type TableProps<T, K extends keyof T> = {
   data: Array<T>;
+  setData?: Dispatch<SetStateAction<Array<T>>>;
   columns: Array<ColumnDefinitionType<T, K>>;
+  showIdx?: boolean;
   type?: keyof SortOption;
+  dragable?: boolean;
+  sort?: boolean;
+  mark?: number;
 };
 
 type TableHeaderProps<T, K extends keyof T> = {
   columns: Array<ColumnDefinitionType<T, K>>;
   setColumn: (column: SortColumn<T, K>) => void;
+  showIdx?: boolean;
 };
 
 type TableRowsProps<T, K extends keyof T> = {
   data: Array<T>;
+  setData?: Dispatch<SetStateAction<Array<T>>>;
   columns: Array<ColumnDefinitionType<T, K>>;
   selectedColumn: SortColumn<T, K>;
+  showIdx?: boolean;
+  dragble?: boolean;
+  sort?: boolean;
+  mark?: number;
 };
 
 type SortColumn<T, K extends keyof T> = {
@@ -34,7 +51,15 @@ type SortColumn<T, K extends keyof T> = {
   sortType: keyof SortOption;
 };
 
-const Table = <T, K extends keyof T>({ data, columns }: TableProps<T, K>) => {
+const Table = <T, K extends keyof T>({
+  data,
+  setData,
+  columns,
+  showIdx = false,
+  dragable = false,
+  sort = true,
+  mark,
+}: TableProps<T, K>) => {
   const initColumn = {
     columnName: columns[0].cell as K,
     // defaults to number as its a straight comparison a < b
@@ -45,11 +70,20 @@ const Table = <T, K extends keyof T>({ data, columns }: TableProps<T, K>) => {
 
   return (
     <table className={'table'}>
-      <TableHeader columns={columns} setColumn={setSelectedColumn} />
+      <TableHeader
+        columns={columns}
+        setColumn={setSelectedColumn}
+        showIdx={showIdx}
+      />
       <TableRows
         data={data}
+        setData={setData}
         columns={columns}
         selectedColumn={selectedColumn}
+        showIdx={showIdx}
+        dragble={dragable}
+        sort={sort}
+        mark={mark}
       />
     </table>
   );
@@ -58,6 +92,7 @@ const Table = <T, K extends keyof T>({ data, columns }: TableProps<T, K>) => {
 const TableHeader = <T, K extends keyof T>({
   columns,
   setColumn,
+  showIdx,
 }: TableHeaderProps<T, K>) => {
   const onColumnClick = (column: ColumnDefinitionType<T, K>) => {
     const selected = {
@@ -70,6 +105,7 @@ const TableHeader = <T, K extends keyof T>({
   return (
     <thead>
       <tr>
+        {showIdx && <th>{'Indeks'}</th>}
         {columns.map((column, index) => {
           return (
             <th key={`headCell-${index}`} onClick={() => onColumnClick(column)}>
@@ -84,10 +120,17 @@ const TableHeader = <T, K extends keyof T>({
 
 const TableRows = <T, K extends keyof T>({
   data,
+  setData,
   columns,
   selectedColumn,
+  showIdx = false,
+  dragble = false,
+  sort = true,
+  mark,
 }: TableRowsProps<T, K>) => {
   const [sortedData, setSortedData] = useState<Array<T>>(data);
+  const [pos, setPos] = useState<number | undefined>();
+  const [dragOver, setDragover] = useState<number | undefined>();
 
   function OrderByArray(values: Array<T>, orderType: K) {
     const func = typeToFunction[selectedColumn.sortType];
@@ -96,18 +139,58 @@ const TableRows = <T, K extends keyof T>({
   }
 
   useEffect(() => {
+    if (setData) {
+      setData(sortedData);
+    }
+  }, [sortedData]);
+
+  useEffect(() => {
+    if (sort === false) {
+      return;
+    }
     OrderByArray(sortedData, selectedColumn.columnName as K);
   }, [selectedColumn]);
 
   useEffect(() => {
+    if (sort === false) {
+      setSortedData(data);
+      return;
+    }
     OrderByArray(data, selectedColumn.columnName as K);
   }, [data]);
+
+  const dragStart = (_e: DragEvent<HTMLDivElement>, position: number) => {
+    setPos(position);
+  };
+
+  const dragEnter = (_e: DragEvent<HTMLDivElement>, position: any) => {
+    setDragover(position);
+  };
+
+  const drop = () => {
+    const copyListItems = [...sortedData];
+    if (pos !== undefined && dragOver !== undefined) {
+      const dragItemContent = copyListItems[pos];
+      copyListItems.splice(pos, 1);
+      copyListItems.splice(dragOver, 0, dragItemContent);
+    }
+    setPos(undefined);
+    setDragover(undefined);
+    setSortedData(copyListItems);
+  };
 
   return (
     <tbody>
       {sortedData.map((row, index) => {
         return (
-          <tr key={`row-${index}`}>
+          <tr
+            key={`row-${index}`}
+            draggable={dragble}
+            onDragStart={(e) => dragStart(e, index)}
+            onDragEnter={(e) => dragEnter(e, index)}
+            onDragEnd={drop}
+            className={index === mark ? 'marked' : undefined}>
+            {showIdx && <td key={index}>{index}</td>}
             {columns.map((column, index2) => {
               return (
                 <td key={`cell-${index2}`}>
