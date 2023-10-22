de52d6a803d44a7a435ae2ecac63b683d605a994
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2023-10-22 02:01:24 +0200

    :chart_with_upwards_trend: add endpoints for tracking unique visits and page visits

---
+++ src/api/stats.ts
@@ -0,0 +1,22 @@
+import { IPageStats, IPageVisit, IVisits } from 'models/apiModels';
+import { get, post } from './requests';
+
+export const postUniqueVisit = (): Promise<{}> =>
+  post<{}>('stats/unique-visit', {});
+
+export const getUniqueVisits = (
+  start?: string,
+  end?: string
+): Promise<IVisits[]> => {
+  return get<IVisits[]>('stats/unique-visit', {
+    ...(start && { start: start }),
+    // endpoint uses datenow if end is not specified
+    ...(end && { end: end }),
+  });
+};
+
+export const logVisit = (payload: IPageVisit): Promise<{}> =>
+  post<{}>('stats/page-visit', payload);
+
+export const getPageVisitsLastMonth = (): Promise<IPageStats[]> =>
+  get<IPageStats[]>('stats/most_visited_pages_last_month');
