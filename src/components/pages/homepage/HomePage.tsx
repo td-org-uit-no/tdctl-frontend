b0bf02a47df606c643e046b85edca7618d98d811
Ole Tytlandsvik <ole.tytlandsvik@gmail.com>
2023-12-21 00:46:57 +0100

    :lipstick: Redesign front page with Chakra-ui

--- src/components/pages/homepage/HomePage.tsx
+++ src/components/pages/homepage/HomePage.tsx
@@ -1,13 +1,45 @@
 import useTitle from 'hooks/useTitle';
 import HomeHeader from 'components/molecules/homePage/header/HomeHeader';
 import Footer from 'components/molecules/footer/Footer';
-import styles from './homePage.module.scss';
 import Carousel from 'components/molecules/carousel/Carousel';
 import EventPreview from 'components/molecules/event/eventPreview/EventPreview';
 import useUpcomingEvents from 'hooks/useEvents';
 import LoadingWrapper from 'components/atoms/loadingWrapper/LoadingWrapper';
 import { useMobileScreen } from 'hooks/useMobileScreen';
 import NoUpcomingEvents from 'components/molecules/homePage/noUpcomingEventResponse/evnetResponse';
+import { Flex, VStack, Heading, Text, Box, Center } from '@chakra-ui/react';
+import Icon from 'components/atoms/icons/icon';
+
+interface NavCardProps {
+  header: string;
+  linkTo: string;
+  children?: React.ReactNode;
+}
+
+const NavCard: React.FC<NavCardProps> = ({ header, linkTo, children }) => {
+  return (
+    <Flex
+      direction={'row'}
+      justify={'space-between'}
+      bg={'slate.700'}
+      _hover={{ bg: 'slate.600' }}
+      rounded={'lg'}
+      shadow={'md'}
+      cursor={'pointer'}
+      w={'100%'}
+      p={'1rem'}>
+      <VStack align={'start'}>
+        <Heading size={'md'} p={0} m={0}>
+          {header}
+        </Heading>
+        <Text fontSize={'.75rem'} p={0} m={0}>
+          {children}
+        </Text>
+      </VStack>
+      <Icon type="arrow-right" size={2} />
+    </Flex>
+  );
+};
 
 const RootPage = () => {
   useTitle('Tromsøstudentenes-Dataforening');
@@ -15,40 +47,51 @@ const RootPage = () => {
   const isMobile = useMobileScreen();
 
   return (
-    <div className={styles.root}>
+    <VStack>
       <HomeHeader />
-      <div className={styles.eventsContainer}>
-        <div className={styles.eventsWrapper}>
-          <LoadingWrapper
-            data={events}
-            animation={true}
-            startAfter={400}
-            className={styles.wrapper}>
-            {events && events.length ? (
-              <div style={{ width: '75vw' }}>
-                <Carousel
-                  title="Arrangementer"
-                  dir={isMobile ? 'column' : 'row'}
-                  viewItems={isMobile ? 1 : 3}
-                  spacing={!isMobile}
-                  height={'45vh'}>
-                  {events.map((event) => (
-                    <EventPreview
-                      eventData={event}
-                      orientation={'vertical'}
-                      key={event.eid}
-                    />
-                  ))}
-                </Carousel>
-              </div>
-            ) : (
-              <NoUpcomingEvents />
-            )}
-          </LoadingWrapper>
-        </div>
-      </div>
+      {/* <Flex
+        direction={{ base: 'column', lg: 'row' }}
+        justify={'space-between'}
+        width={{ base: '80vw', lg: '60vw' }}
+        gap={{ base: '1rem', lg: '4rem' }}
+        mt={'1rem'}>
+        <NavCard header="Stillingsutlysninger" linkTo="test">
+          Sjekk ut stillingsannonser her
+        </NavCard>
+        <NavCard header="Ny student" linkTo="test">
+          Ofte stilte spørsmål og generell info
+        </NavCard>
+      </Flex> */}
+      <Center w={{ base: '85vw', xl: '75vw' }}>
+        <LoadingWrapper
+          data={events}
+          animation={true}
+          startAfter={400}
+          className={styles.wrapper}>
+          {events && events.length ? (
+            <Box w={'100%'}>
+              <Carousel
+                title="Arrangementer"
+                dir={isMobile ? 'column' : 'row'}
+                viewItems={isMobile ? 1 : 3}
+                spacing={!isMobile}
+                height={'45vh'}>
+                {events.map((event) => (
+                  <EventPreview
+                    eventData={event}
+                    orientation={'vertical'}
+                    key={event.eid}
+                  />
+                ))}
+              </Carousel>
+            </Box>
+          ) : (
+            <NoUpcomingEvents />
+          )}
+        </LoadingWrapper>
+      </Center>
       <Footer />
-    </div>
+    </VStack>
   );
 };
 
